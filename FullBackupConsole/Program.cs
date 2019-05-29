0d3069da5339d9baeb3f1cb4d0c7a440fdbdf975
Pablo Biajoni <pbiajoni@hotmail.com>
2019-05-29 17:27:33 -0300

    Commit final do dia

--- FullBackupConsole/Program.cs
+++ FullBackupConsole/Program.cs
@@ -12,11 +12,108 @@ namespace FullBackupConsole
         //-sourceC://Caminho/do/diretorio/raiz
         //-targetD://Caminho/do/diretorio/destino
         //-z para zipar
-        //-ignorePasta1,Pasta2,Pasta3
+        //-ignore[Pasta1,Pasta2,Pasta3]
+        //-ignoreext[exe,mp3,mp4]
         static string log = "";
+        static List<DirectoryInfo> directoriesToCopy = new List<DirectoryInfo>();
         static void Main(string[] args)
         {
+            if (!ArgExists("-source", args))
+            {
+                throw new Exception("É necessário o argumento -sourceC://Caminho/do/diretorio/raiz");
+            }
 
+            if (!ArgExists("-target", args))
+            {
+                throw new Exception("É necessário o argumento -targetD://Caminho/do/diretorio/destino");
+            }
+
+            string rootPath = GetArg("-source", args);
+            string targetPath = GetArg("-target", args);
+            string ignoredFolders = GetArg("-ignore", args);
+
+            foreach (string dir in Directory.GetDirectories(rootPath))
+            {
+                if (!IsIgnoredFolder(dir, args))
+                {
+                    directoriesToCopy.Add(new DirectoryInfo(Path.Combine(rootPath, dir)));
+                }
+            }
+
+            string targetBackupPath = GetBackupFolderName(targetPath);
+
+            foreach (DirectoryInfo directoryInfo in directoriesToCopy)
+            {
+                Copy(directoryInfo.FullName, targetPath,args);
+            }
+        }
+
+        static void Log(string text)
+        {
+            log += DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss") + " - " + text + Environment.NewLine;
+            Console.WriteLine(text);
+        }
+
+        static bool IsIgnoredFolder(string folderName, string[] args)
+        {
+            if (ArgExists("-ignore", args))
+            {
+                string ign = GetArg("-ignore", args);
+                ign = ign.Replace("[", "").Replace("]", "");
+                string[] igns = ign.Split(',');
+
+                foreach (string folder in igns)
+                {
+                    if (folderName.ToLower().Trim() == folder.ToLower().Trim())
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        static bool IsIgnoredFile(string fileName, string[] args)
+        {
+            if (ArgExists("-ignoreext", args))
+            {
+                string ign = GetArg("-ignoreext", args);
+                ign = ign.Replace("[", "").Replace("]", "");
+                string[] igns = ign.Split(',');
+
+                foreach (string fileext in igns)
+                {
+                    string[] ext = fileName.Split('.');
+
+                    if (ext.Length == 2)
+                    {
+                        if (ext[1].ToLower().Trim() == fileext.ToLower().Trim())
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        static string GetBackupFolderName(string targetPath)
+        {
+            DateTime today = DateTime.Now;
+            string folderName = today.Day + "_" + today.Month + "_" + today.Year;
+            string path = Path.Combine(targetPath, folderName);
+            string hourPath = "";
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+                hourPath = Path.Combine(path, (today.Hour + "" + today.Minute + "" + today.Second));
+                Directory.CreateDirectory(hourPath);
+            }
+
+            return hourPath;
         }
 
         static string ToSize(long source)
@@ -41,11 +138,7 @@ namespace FullBackupConsole
                 return string.Concat(bytes, " Bytes");
             }
         }
-        static void Log(string text)
-        {
-            log += DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss") + " - " + text + Environment.NewLine;
-            Console.WriteLine(text);
-        }
+
         static string GetArg(string param, string[] args)
         {
             foreach (string arg in args)
@@ -72,23 +165,32 @@ namespace FullBackupConsole
             return false;
         }
 
-        public static void Copy(string sourceDirectory, string targetDirectory)
+        public static void Copy(string sourceDirectory, string targetDirectory, string[] args)
         {
             DirectoryInfo diSource = new DirectoryInfo(sourceDirectory);
             DirectoryInfo diTarget = new DirectoryInfo(targetDirectory);
 
-            CopyAll(diSource, diTarget);
+            CopyAll(diSource, diTarget, args);
         }
 
-        public static void CopyAll(DirectoryInfo source, DirectoryInfo target)
+        public static void CopyAll(DirectoryInfo source, DirectoryInfo target, string[] args)
         {
             Directory.CreateDirectory(target.FullName);
 
             // Copy each file into the new directory.
             foreach (FileInfo fi in source.GetFiles())
             {
-                Console.WriteLine(@"Copiando {0}\{1}", target.FullName, fi.Name);
-                fi.CopyTo(Path.Combine(target.FullName, fi.Name), true);
+                string fullFileName = fi.Name + "." + fi.Extension;
+
+                if (!IsIgnoredFile(fullFileName, args))
+                {
+                    Console.WriteLine(@"Copiando {0}\{1}", target.FullName, fi.Name);
+                    fi.CopyTo(Path.Combine(target.FullName, fi.Name), true);
+                }
+                else
+                {
+
+                }
             }
 
             // Copy each subdirectory using recursion.
@@ -96,7 +198,7 @@ namespace FullBackupConsole
             {
                 DirectoryInfo nextTargetSubDir =
                     target.CreateSubdirectory(diSourceSubDir.Name);
-                CopyAll(diSourceSubDir, nextTargetSubDir);
+                CopyAll(diSourceSubDir, nextTargetSubDir, args);
             }
         }
     }
